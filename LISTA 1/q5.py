95a447ea319a62e476422b981940bae8f8d0a350
l3omc <leo.carvalho95@yahoo.com.br>
2023-02-27 13:03:06 -0300

    atualizando listas

---
+++ LISTA 1/q5.py
@@ -0,0 +1,9 @@
+#Faça um Programa que converta metros para centímetros.
+
+
+n1 = input("Digite o valor em metros que deseja converter para centimetros: ")
+print("seu número é ", n1)
+
+n1 = int(n1)
+cent = (n1)*100
+print("A consão do número é %f centimetros", cent)
